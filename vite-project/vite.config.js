0fe3cc27a96141a43cd5118f5bfe6e1d19d6c7d8
yael-avi <yaela@tlp-ins.co.il>
2026-03-11 13:03:38 +0200

    fix vite config -- port

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(),], preview: {
-        allowedHosts: ['ifo.onrender.com']
-    },
-    server: {
+        allowedHosts: ['ifo.onrender.com'],
         host: true,
         strictPort: true,
         port: 3000,
     },
+
 })
